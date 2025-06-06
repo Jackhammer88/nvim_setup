87f1297b0045b567e6286ff96e3b30b56f272939
Jackhammer88 <jackhammer88@users.noreply.github.com>
2025-06-06 10:41:59 +0300

    Initial commit

---
+++ lua/options.lua
@@ -0,0 +1,86 @@
+require "nvchad.options"
+
+local opt = vim.opt
+
+-- Подключение системного буфера обмена
+opt.clipboard:append("unnamedplus")
+
+-----------------------------------------------------------
+-- ОБЩИЕ ОПЦИИ
+-----------------------------------------------------------
+opt.mouse = 'a'              --Включит мышь
+opt.encoding = 'utf-8'       --Кодировка
+opt.showcmd = true           --Отображение команд
+vim.cmd([[
+filetype indent plugin on
+syntax enable
+]])
+opt.swapfile = false         --Не создаем свап файлы
+
+-----------------------------------------------------------
+-- ВИЗУАЛЬНЫЕ ОПЦИИ
+-----------------------------------------------------------
+opt.number = true            --Номер строк сбоку
+opt.wrap = true              --Длинные линии будет видно
+opt.expandtab = true         --???
+opt.tabstop = 4              --1 tab = 4 пробела
+opt.smartindent = true
+opt.shiftwidth = 4           --Смещаем на 4 пробела
+
+-- 2 spaces for selected filetypes
+vim.cmd [[
+autocmd FileType xml,html,xhtml,css,scss,javascript,lua,yaml,htmljinja setlocal shiftwidth=2 tabstop=2
+]]
+
+opt.so = 5                   --Отступ курсора от края экрана
+opt.foldcolumn = '2'         --Ширина колонки для фолдов
+opt.colorcolumn =  '119'     --Расположение цветной колонки
+
+-- remove line lenght marker for selected filetypes
+vim.cmd [[autocmd FileType text,markdown,html,xhtml,javascript setlocal cc=0]]
+
+opt.cursorline = true        -- Подсветка строки с курсором
+opt.termguicolors = true
+
+-- Компактный вид у тагбара и Отк. сортировка по имени у тагбара
+vim.g.tagbar_compact = 1
+vim.g.tagbar_sort = 0
+
+-----------------------------------------------------------
+-- НАСТРОЙКИ ПОИСКА
+-----------------------------------------------------------
+-- Будет игнорировать размер букв при поиске
+opt.ignorecase = true        --Игнорировать размер букв
+opt.smartcase = true         --Игнор прописных буквj
+
+-----------------------------------------------------------
+-- НАСТРОЙКИ ПЛАГИНОВ
+-----------------------------------------------------------
+
+-- nvim-cmp supports additional completion capabilities
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+vim.o.completeopt = 'menuone,noselect'
+-- luasnip setup
+local luasnip = require 'luasnip'
+-- nvim-cmp setup
+local cmp = require 'cmp'
+cmp.setup {
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end,
+    },
+    sources = {
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+        { name = 'path' },
+        { name = 'buffer', option = {
+            get_bufnrs = function()
+                return vim.api.nvim_list_bufs()
+            end
+        },
+    },
+},
+}
+
