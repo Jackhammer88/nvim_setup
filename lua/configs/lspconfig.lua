c3cadb8125fa8359d7be565996d7e4f6271c3c44
Jackhammer88 <jackhammer88@users.noreply.github.com>
2026-01-08 18:19:41 +0300

    Omnisharp fixed

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -3,10 +3,35 @@
 local nvlsp = require "nvchad.configs.lspconfig"
 nvlsp.defaults()
 
+local mason_omnisharp = vim.fn.stdpath("data") .. "/mason/bin/omnisharp"
+
+local function omnisharp_cmd()
+  local pid = tostring(vim.fn.getpid())
+  local args = {
+    "-z",
+    "--languageserver",
+    "--hostPID", pid,
+    "--encoding", "utf-8",
+    "DotNet:enablePackageRestore=false",
+  }
+
+  if vim.fn.filereadable(mason_omnisharp) == 1 then
+    return vim.list_extend({ mason_omnisharp }, args)
+  end
+
+  if vim.fn.executable("OmniSharp") == 1 then
+    return vim.list_extend({ "OmniSharp" }, args)
+  end
+
+  return vim.list_extend({ "omnisharp" }, args)
+end
+
 local servers = {
   html = {},
   cssls = {},
-  omnisharp = {},
+  omnisharp = {
+    cmd = omnisharp_cmd(),
+  },
 }
 
 for server, opts in pairs(servers) do
